dba8e6851f6f431167863c0b5ab9e5a301239806
dragana.bojic <job.dragana@gmail.com>
2023-02-12 21:36:17 +0100

    security htmlspecialchars

--- login_request.php
+++ login_request.php
@@ -2,14 +2,16 @@
     if(isset($_POST['username'])){
         
         $db = new mysqli("localhost", "root", "", "store");
+        $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
+        $password = htmlspecialchars($_POST["password"], ENT_QUOTES);
         $sql_query = 
         "SELECT * FROM `user`
-                 WHERE `UserName` = '$_POST[username]'";
+                 WHERE `UserName` = '$username'";
 
         $response = $db->query($sql_query);
 
-        if($response->num_rows == 1 && $response->fetch_assoc()['Password'] == $_POST['password']){
-            setcookie('username', $_POST['username'], time() + 3600);
+        if($response->num_rows == 1 && $response->fetch_assoc()['Password'] == $password){
+            setcookie('username', $username, time() + 3600);
              header("Location: index.php");
         }
 
--- register_request.php
+++ register_request.php
@@ -1,22 +1,24 @@
 <?php 
     if(isset($_POST['username'])){
         $db = new mysqli("localhost", "root", "", "store");
+        $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
+        $password = htmlspecialchars($_POST["password"], ENT_QUOTES);
 
         $sql_query = 
                  "SELECT `UserName` 
                  FROM `user` 
-                 WHERE `UserName` = '$_POST[username]'";
+                 WHERE `UserName` = '$username'";
 
         $response = $db->query($sql_query);
     
         if($response->num_rows === 0){
             $sql_query =
              "INSERT INTO `user` (`UserName`,`Password`) 
-             VALUES ('$_POST[username]', '$_POST[password]')";
+             VALUES ('$username', '$password')";
     
             $db->query($sql_query);
         
-            setcookie('username', $_POST['username'], time() + 3600);
+            setcookie('username', $username, time() + 3600);
             header("Location: index.php");
 
         
