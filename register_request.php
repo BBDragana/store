cbdb37922c06102ac6a76afbdf422b747bc0c9fc
dragana.bojic <job.dragana@gmail.com>
2023-02-14 00:10:26 +0100

    create admin

--- register_request.php
+++ register_request.php
@@ -1,31 +1,36 @@
-<?php 
-    if(isset($_POST['username'])){
-        $db = new mysqli("localhost", "root", "", "store");
-        $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
-        $password = password_hash(htmlspecialchars($_POST["password"], ENT_QUOTES), PASSWORD_DEFAULT);
+<?php
+if (isset($_POST['username'])) {
+    $db = new mysqli("localhost", "root", "", "store");
+    $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
+    $password = password_hash(htmlspecialchars($_POST["password"], ENT_QUOTES), PASSWORD_DEFAULT);
 
-        $sql_query = 
-                 "SELECT `UserName` 
+    $sql_query =
+        "SELECT `UserName` 
                  FROM `user` 
                  WHERE `UserName` = '$username'";
 
+    $response = $db->query($sql_query);
+
+    if ($response->num_rows === 0) {
+        $sql_query =
+            "SELECT COUNT(UserId) FROM `user`";
         $response = $db->query($sql_query);
-    
-        if($response->num_rows === 0){
+        if ($response->fetch_assoc()["COUNT(UserId)"] == 0) {
             $sql_query =
-             "INSERT INTO `user` (`UserName`,`Password`) 
-             VALUES ('$username', '$password')";
-    
-            $db->query($sql_query);
-        
-            setcookie('username', $username, time() + 3600);
-            header("Location: index.php");
-
-          $db->close();
-        }
-        else{
-        header("Location: account_already_exists.php");            
+                "INSERT INTO `user` (`UserName`,`Password`, `RoleId`) 
+             VALUES ('$username', '$password', 1)";
+        } else {
+            $sql_query =
+                "INSERT INTO `user` (`UserName`,`Password`, `RoleId`) 
+             VALUES ('$username', '$password', 2)";
         }
 
-          
+        $db->query($sql_query);
+
+        setcookie('username', $username, time() + 3600);
+        header("Location: index.php");
+    } else {
+        header("Location: account_already_exists.php");
     }
+    $db->close();
+}
